daf9d7453eb457e6e3633bafea40d6151dad9cb1
Vipulkumar98 <vipulkumar98@users.noreply.github.com>
2025-10-02 19:50:32 +0530

    Remove __pycache__ from repo and updated init.py

--- board/__init__.py
+++ board/__init__.py
@@ -1,5 +1,7 @@
+import os
 
 
+from dotenv import load_dotenv
 from flask import Flask
 
 from board import pages, posts
@@ -14,10 +16,17 @@ from board import pages, posts
 # if __name__ == '__main__':
 #     app.run(host='0.0.0.0', port=8000, debug=True) 
 
+load_dotenv()
+
 def create_app():
     app = Flask(__name__)
+    app.config.from_prefixed_env()
 
     app.register_blueprint(pages.bp)
     app.register_blueprint(posts.bp)
+
+    print(f"Current Environment: {os.getenv('ENVIRONMENT')}")
+
+    print(f"Using Database: {app.config.get('DATABASE')}")
 
     return app
